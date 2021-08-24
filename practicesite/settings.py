79b341399032cf8b70d89bc5b451f10c293cf2bd
Aishika Midder <aishikamidder50@gmail.com>
2021-08-25 02:54:50 +0530

    heroku

--- practicesite/settings.py
+++ practicesite/settings.py
@@ -125,8 +125,6 @@ STATIC_URL = '/static/'
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 django_heroku.settings(locals())
 
