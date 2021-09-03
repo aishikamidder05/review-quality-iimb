2e646729a27eb10853fb1fcd975a7e52c8312c15
Aishika Midder <aishikamidder50@gmail.com>
2021-09-04 02:30:19 +0530

    export csv

--- newapp1/admin.py
+++ newapp1/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import  UserReview
+from csvexport.actions import csvexport
 # Register your models here.
-admin.site.register(UserReview)
+
+class UserReviewAdmin(admin.ModelAdmin):
+    actions = [csvexport]
+    list_display = ('prolific_id', 'star_rating', 'review_heading', 'review_box', 'review_depth', 'ari', 'cli', 'avg')
+
+admin.site.register(UserReview, UserReviewAdmin)
