92a110c563e95fe164f6bf337a9b915ffe713548
Aishika Midder <aishikamidder50@gmail.com>
2021-09-18 03:24:42 +0530

    paths set

--- newapp1/urls.py
+++ newapp1/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 from django.conf import settings
@@ -8,8 +9,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     
     path('', views.index, name='home'),
-    path('exit', views.exit, name="exit")
-
+    path('exit', views.exit, name="exit"),
+    path('getindices', csrf_exempt(views.returnIndices), name='indices'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
