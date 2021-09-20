fb331315735435c9b2c9edea66781922b7fb7f04
Aishika Midder <aishikamidder50@gmail.com>
2021-09-21 02:07:52 +0530

    models

--- newapp1/models.py
+++ newapp1/models.py
@@ -16,3 +16,35 @@ class UserReview(models.Model):
 
     def __str__(self):
         return self.prolific_id
+
+
+class UserReviewNoIndicator(models.Model):
+    star_rating = models.IntegerField(null=True, blank=True)
+    review_heading = models.CharField(max_length=300)
+    review_box = models.TextField()
+    review_depth = models.IntegerField(null=True, blank=True)
+    ari = models.DecimalField(null=True, blank=True, max_digits=10, decimal_places=5)
+    cli = models.DecimalField(null=True, blank=True, max_digits=10, decimal_places=5)
+    avg = models.DecimalField(null=True, blank=True, max_digits=10, decimal_places=5) 
+    prolific_id = models.CharField(max_length=300, null=True, blank=True)
+
+
+
+    def __str__(self):
+        return self.prolific_id
+
+
+class UserReview500char(models.Model):
+    star_rating = models.IntegerField(null=True, blank=True)
+    review_heading = models.CharField(max_length=300)
+    review_box = models.TextField()
+    review_depth = models.IntegerField(null=True, blank=True)
+    ari = models.DecimalField(null=True, blank=True, max_digits=10, decimal_places=5)
+    cli = models.DecimalField(null=True, blank=True, max_digits=10, decimal_places=5)
+    avg = models.DecimalField(null=True, blank=True, max_digits=10, decimal_places=5) 
+    prolific_id = models.CharField(max_length=300, null=True, blank=True)
+
+
+
+    def __str__(self):
+        return self.prolific_id
