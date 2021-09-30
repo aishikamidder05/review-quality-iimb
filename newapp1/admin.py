285ac27461fa7e2e0aa5e28b2f51516872edc9db
Aishika Midder <aishikamidder50@gmail.com>
2021-10-01 02:44:17 +0530

    registered

--- newapp1/admin.py
+++ newapp1/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import  UserReview , UserReviewNoIndicator, UserReview500char
+from .models import  UserReview , UserReviewNoIndicator, UserReview500char, RestModel
 from csvexport.actions import csvexport
 # Register your models here.
 
@@ -22,3 +22,9 @@ class UserReview500charAdmin(admin.ModelAdmin):
     list_display = ('prolific_id', 'star_rating', 'review_heading', 'review_box', 'review_depth', 'ari', 'cli', 'avg')
 
 admin.site.register(UserReview500char, UserReview500charAdmin)
+
+class RestModelAdmin(admin.ModelAdmin):
+  
+    list_display = ('key', 'value')
+
+admin.site.register(RestModel, RestModelAdmin)
